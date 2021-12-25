18117c6c02cf8b990077a2544ce82c38e1163dd7
Aditilal <68782932+Aditilal@users.noreply.github.com>
2021-12-25 15:25:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+require("discord-reply"); // Initializing Discord-Reply
+const bot = new Discord.Client();
+
+const Tesseract = require("tesseract.js"); // Initializing Tesseract, this module is essential for OCR
+
+bot.on("ready", () => {
+  console.log(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on("message", (msg) => {
+  if (msg.attachments.size > 0) {
+    msg.attachments.forEach((attachment) => {
+      // Getting the Image URL
+      var ImageURL = attachment.proxyURL;
+
+      // Running the image through Tesseract
+      Tesseract.recognize(
+        ImageURL,
+        "eng",
+        { logger: (m) => console.log(m) }
+      ).then(({ data: { text } }) => {
+        // Replying with the extracted test
+        console.log(text);
+        msg.reply(text);
+      });
+    });
+  }
+});
+console.log("Working");
+
+bot.login("OTI0MTk5MTYwOTY0NjAzOTI0.YcbFig.EUGH_VBd0-POt-GyuvbDwVVVbuU");
+
+
